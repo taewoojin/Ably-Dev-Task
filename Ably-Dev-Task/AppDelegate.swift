66e655e4ebda90b96b6023910ed1a5182b8ef310
taewoo <qkqnrpa@gmail.com>
2022-06-18 19:02:52 +0900

    [feature] 프로젝트 폴더 구조 구성, 초기 화면(tabBar) 구현

--- Ably-Dev-Task/AppDelegate.swift
+++ Ably-Dev-Task/AppDelegate.swift
@@ -7,30 +7,56 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // MARK: Properties
+    
+    var window: UIWindow?
+    
+    let rootView: UINavigationController = UINavigationController().then {
+        $0.setNavigationBarHidden(true, animated: false)
+    }
+    
+    lazy var tabBarCoordinator = TabBarCoordinator(presenter: rootView)
+    
+    lazy var homeCoordinator = HomeCoordinator(presenter: rootView)
+    
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = rootView
+        self.window?.makeKeyAndVisible()
+        
+        tabBarCoordinator.start()
+        applyAppearance()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    private func applyAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .systemGray6
+        
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+//        UINavigationBar.appearance().tintColor = .clear
+//        UINavigationBar.appearance().isTranslucent = true
+        
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
---
+++ Ably-Dev-Task/Source/Present/BaseViewController.swift
@@ -0,0 +1,110 @@
+//
+//  BaseViewController.swift
+//  Ably-Dev-Task
+//
+//  Created by 진태우 on 2022/06/18.
+//
+
+import UIKit
+
+import RxSwift
+
+
+class BaseViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    lazy private(set) var className: String = {
+        return type(of: self).description().components(separatedBy: ".").last ?? ""
+    }()
+    
+    weak var coordinatorDelegate: CoordinatorDidFinishDelegate?
+    
+    var disposeBag = DisposeBag()
+    
+    
+    // MARK: Layout Constraints
+    
+    private(set) var didSetupConstraints = false
+    
+    
+    // MARK: Initializing
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        setupLifeCycleBinding()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setupLifeCycleBinding()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupLifeCycleBinding()
+    }
+    
+    deinit {
+        coordinatorDelegate?.didFinishCoordinator()
+        debugPrint("DEINIT: \(self.className)")
+    }
+    
+    
+    // MARK: Life Cycle Views
+    
+    override func viewDidLoad() {
+        setupAttributes()
+        setupLayout()
+        setupLocalization()
+        setupBinding()
+        setData()
+        self.view.setNeedsUpdateConstraints()
+    }
+    
+    override func updateViewConstraints() {
+        if !self.didSetupConstraints {
+            self.setupConstraints()
+            self.didSetupConstraints = true
+        }
+        super.updateViewConstraints()
+    }
+    
+    
+    // MARK: Setup
+    
+    func setupLayout() {
+        // Override Layout
+    }
+    
+    func setupConstraints() {
+        // Override Constraints
+    }
+    
+    func setupAttributes() {
+        // Override Attributes
+//        view.backgroundColor = .black
+//        view.backgroundColor = Color.mainContainerBackgroundColor
+    }
+    
+    func setupLocalization() {
+        // Override Localization
+    }
+    
+    func setupLifeCycleBinding() {
+        // Override Binding for Lify Cycle Views
+    }
+    
+    func setupBinding() {
+        // Override Binding
+    }
+    
+    func setData() {
+        // Override Set Data
+    }
+    
+    
+    // MARK: Local Methods
+    
+}
+
